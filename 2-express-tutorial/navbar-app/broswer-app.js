4cc7f70a8fcf86ff047e5aed40d48610a90655d7
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-06-04 10:27:42 +0100

    frontend update2

---
+++ 2-express-tutorial/navbar-app/broswer-app.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.nav-toggle')
+const toggleNavLinks = document.querySelector('.nav-link')
+const navLink = document.querySelector('.nav-link2')
+const toggleMenu = document.querySelector('.nav-toggle-img')
+
+
+btn.addEventListener('click', () =>{
+    toggleNavLinks.classList.toggle('nav-link2');
+    toggleMenu.style.transform = "rotate(90deg)";
+  
+
+})
