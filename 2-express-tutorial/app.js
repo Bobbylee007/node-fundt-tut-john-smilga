375e3e549d3bcc285d01a489a3d988593824b281
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-06-13 09:54:08 +0100

    work with Html data

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -5,14 +5,32 @@ let {people} = require('./data')
 // static assets
 app.use(express.static('./methods-public'))
 
+//parse form data
+app.use(express.urlencoded({entended: false}))
+
 //read data
 app.get('/api/people', (req, res) => {
   res.status(200).json({success:true, data:people})
 })
 
+app.post('/api/people', (req, res) => {
+  res.status(201).send('success')
+})
+
 //add or insert data
 //two flavors
+// 1* using HTML forms and  parse the form with express middleware urlencoded to get form data
+// 2* using javascript to send request
 
+app.post('/login', (req, res) => {
+  // console.log(req.body)
+  const {name} = req.body
+  if(name){
+    return res.status(200).send(`Welcome ${name}`)
+  }
+  res.status(401).send('please provide credentials')
+  res.send('POST')
+})
 
 
 app.listen(5000, () => {
