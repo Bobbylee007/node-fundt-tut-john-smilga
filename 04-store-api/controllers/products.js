290a72b550eb43814342f1e528f7bf61ed8bc8a3
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-07-28 10:29:14 +0100

    filter2

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -1,13 +1,13 @@
 const product = require('../models/product')
-const Product = require('../models/product')
 
 
 // manual approach
 const getAllProductsStatic = async  (req, res) => {
-    const products = await Product.find({
-        featured: true,
+    const search = 'ab'
+    const products = await product.find({
+        name: { $regex: search, $options: 'i'}
     })
-    res.status(200).json({products})
+    res.status(200).json({products, nbHits: products.length})
     // throw new Error('testing async Errors')
     // res.status(200).json({msg:'products testing route'})
 }
@@ -15,9 +15,25 @@ const getAllProductsStatic = async  (req, res) => {
 
 // real functionality
 const getAllProducts = async  (req, res) => {
-    res.status(200).json({msg:'products  route'})
-}
+    const {featured, company, name } = req.query
+    const queryObject = {}
+   
+    if (featured) {
+        queryObject.featured = featured === 'true'? true : false
+    }
 
+    if(company) {
+        queryObject.company = company   
+    }
+
+    if(name) {
+        queryObject.name =  { $regex: name, $options: 'i'}
+    }
+    console.log(queryObject);
+    
+     const products = await product.find(queryObject);
+    res.status(200).json({ products, nbHits: products.length })
+}
 module.exports = {
     getAllProductsStatic, 
     getAllProducts,
