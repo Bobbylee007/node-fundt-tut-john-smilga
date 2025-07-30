6a79e6e6bf378623188d46b0d0cd28f0f60f7c46
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-07-30 10:56:16 +0100

    selection/fields

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -2,7 +2,11 @@ const product = require("../models/product");
 
 // manual approach
 const getAllProductsStatic = async (req, res) => {
-  const products = await product.find({}).sort("-name price");
+  const products = await product.find({})
+  .sort('name')
+  .select('name price')
+  .limit(10)
+  .skip(5)
   res.status(200).json({ products, nbHits: products.length });
   // throw new Error('testing async Errors')
   // res.status(200).json({msg:'products testing route'})
@@ -10,7 +14,7 @@ const getAllProductsStatic = async (req, res) => {
 
 // real functionality
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -24,15 +28,35 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
+  // console.log(queryObject);
 
-  let result = await product.find(queryObject)
+  let result =  product.find(queryObject)
+  // sort
   if (sort) {
-    proucts = products.sort()
+  const sortList = sort.split(',').join(' ');
+  result = result.sort(sortList)
   }
+  else{
+    result = result.sort('creatAt')
+  }
+
+  // select or fields
+  if(fields){
+   const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList)
+  }
+
+  // pagination 
+  const page = Number(req.query.page) || 1
   
+
+
+
+  const products = await result
   res.status(200).json({ products, nbHits: products.length });
-};
+
+
+}
 module.exports = {
   getAllProductsStatic,
   getAllProducts,
