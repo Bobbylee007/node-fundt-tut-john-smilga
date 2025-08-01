382a172c522216d55b156a362a583ef5306659c6
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-08-01 10:42:49 +0100

    pagination and Numeric filter

--- 04-store-api/controllers/products.js
+++ 04-store-api/controllers/products.js
@@ -2,19 +2,19 @@ const product = require("../models/product");
 
 // manual approach
 const getAllProductsStatic = async (req, res) => {
-  const products = await product.find({})
+  const products = await product.find({price: { $gt:30 } })
   .sort('name')
   .select('name price')
-  .limit(10)
-  .skip(5)
+  // .limit(10)
+  // .skip(5)
   res.status(200).json({ products, nbHits: products.length });
   // throw new Error('testing async Errors')
   // res.status(200).json({msg:'products testing route'})
 };
 
-// real functionality
+// real functionality (Automated approach)
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -28,7 +28,31 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  // console.log(queryObject);
+
+// Numeric Filters
+if (numericFilters) {
+  const operatorMap = {
+    '>':'$gt',
+    '>=':'$gte',
+    '=':'$wq',
+    '>':'$gt',
+    '<=':'$lte',
+  }
+  const regEx = /\b(<|>|>=|=|<|<=)\b/g
+  let filters = numericFilters.replace(regEx,
+    (match)=>`-${operatorMap[match]}-`
+  )
+  const options = ['price', 'rating'];
+  filters = filters.split(',').forEach((item) => {
+    const [field,operator,value] = item.split('-')
+    if(options.includes(field)){
+      queryObject[field]= {[operator]: Number(value)}
+    }
+  });
+  
+}
+
+  console.log(queryObject);
 
   let result =  product.find(queryObject)
   // sort
@@ -48,10 +72,12 @@ const getAllProducts = async (req, res) => {
 
   // pagination 
   const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page -1) * limit 
   
-
-
-
+  result = result.skip(skip).limit(limit)
+// items 23
+// 4 7 7 7 2     limit page by 7  means  23/7 = 4pages last page will have only 2 items
   const products = await result
   res.status(200).json({ products, nbHits: products.length });
 
