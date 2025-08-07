f91fc4f92cbb12b6d4e05bcc4eac3cb34cb0da0b
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-08-07 11:52:21 +0100

    creating signature with jwt

--- 05-JWT-Basics/controllers/main.js
+++ 05-JWT-Basics/controllers/main.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 // check username, password in post (login) request in req.body
 // if exit create new jwt
 // send back to front-end
 
 // setup authentication so only the request with JWT can access the dashboard
 
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
@@ -13,14 +15,28 @@ const login = async (req, res) => {
     //joi using pakage of joi to validate
     //check in the controller
 
-    if(!usernamen || !password){
+    if(!username || !password){
         throw new CustomAPIError('please provide username and password', 400)
     }
+
+    //just for demo, normally provided by DB!!!
+    const id = new Date().getDate()
+
+    // try to keep payload small, better experience for user
+    //just for demo, in production use long, complex and unguessable string value!!!
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:'30d'})
     
-    res.send('Fake Login/Register/Signup Route')
+    res.status(200).json({msg:'user created',token})
 }
 
 const dashboard = async (req, res) => {
+    const authHeader = req.headers.authorization;
+
+if(!authHeader || !authHeader.startWith('Bearer ')){
+    throw new CustomAPIError('No Token Present', 400)
+}
+    // console.log(req.headers);
+    
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({ msg:`Hello, John Doe`, secret:`Here is your authorized data, your luck number is ${luckyNumber}`})
 }
--- 05-JWT-Basics/public/broswer-app.js
+++ 05-JWT-Basics/public/broswer-app.js
@@ -1,10 +1,60 @@
-const btn = document.querySelector('.nav-toggle')
-const toggleNavLinks = document.querySelector('.nav-link')
-const navLink = document.querySelector('.nav-link2')
-const toggleMenu = document.querySelector('.nav-toggle-img')
+const axios = require('axios')
+const formDom = document.querySelector('.form')
+const usernameInputDom = document.querySelector('.username-input')
+const passwordInputDom = document.querySelector('.password-input')
+const formAlertDom = document.querySelector('.form-alert')
+const resultDom = document.querySelector('.result')
+const btnDom = document.querySelector('#data')
+const tokenDom = document.querySelector('.token')
 
 
-btn.addEventListener('click', () =>{
-    toggleNavLinks.classList.toggle('nav-link2');
-    
+formDom.addEventListener('submit', async (e) => {
+    formAlertDom.classList.remove('text-success')
+    tokenDom.classList.remove('text-success')
+
+    e.preventDefault()
+    const username = usernameInputDom.value
+    const password = passwordInputDom.value
+
+    try {
+        const {data} = await axios.posy('/api/v1/login', { username, password})
+
+        formAlertDom.style.display = 'block'
+        formAlertDom.textContent.display = data.msg
+
+        formAlertDom.classList.add('text-success')
+        usernameInputDom = ''
+        passwordInputDom = ''
+
+        localStorage.setItem('token', data.token)
+        resultDom.innerHTML= ''
+        tokenDom.textContent = 'token present'
+    } catch (error) {
+        formAlertDom.style.display = 'block'
+        formAlertDom.textContent = error.response.data.msg
+        localStorage.removeItem('token')
+        resultDom.innerHTML = ''
+        tokenDom.textContent = 'no token present'
+        tokenDom.classList.remove('text-success')
+        
+    }
+    setTimeout(()=>{
+        formAlertDom.style.display = 'none'
+    }, 2000)    
+})
+
+btnDom.addEventListener('click', async ()=>{
+    const token = localStorage.getItem('token')
+    try {
+        const { data } = await axios.get('/api/v1/dashboard', {
+            Headers: {
+                Authorization: `Beare ${token},`,
+            }, 
+        })
+        resultDom.innerHTML = `<h5>${data.msg}</h5><p>${data.secret}</p>`
+        data.secret
+    } catch (error) {
+        localStorage.removeItem('token')
+    }
+
 })
