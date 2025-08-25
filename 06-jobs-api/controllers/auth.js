b78f87dcfdf8e686773618e979a25bfee7cb3b10
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-08-25 10:33:50 +0100

    hashing password with bcrypt

--- 06-jobs-api/controllers/auth.js
+++ 06-jobs-api/controllers/auth.js
@@ -1,16 +1,15 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
+
 
 
 const register = async (req, res) => {
-    try {
-        const user = await User.create({ ...req.body })
-        res.status(StatusCodes.CREATED).json({ user })
-        
-    } catch (error) {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: error.message  })
-        
-    }
+    
+    const user = await User.create({ ...req.body })
+    res.status(StatusCodes.CREATED).json({ user })   
+
 }
 
 const login = async (req, res) => { 
--- 06-jobs-api/models/User.js
+++ 06-jobs-api/models/User.js
@@ -12,7 +12,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         requiered: [true, 'Please provide eamil'],
         match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:s@"]+)\*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide a valid email'
+        /^\S+@\S+\.\S+$/, 'Please provide a valid email'
       ],
       unique: true,
     },
@@ -20,8 +20,12 @@ const UserSchema = new mongoose.Schema({
         type: String,
         requiered: [true, 'Please provide password'],
         minlength: 6,
-        maxlenght: 12,
+    
     },
 })
 
+UserSchema.pre('save', async function(){
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+})
 module.exports = mongoose.model('User', UserSchema)         
