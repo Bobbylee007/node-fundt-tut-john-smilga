a299241239b8cbd889471011ed523fa0f311846d
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-09-09 10:13:40 +0100

    security/security package installation

--- 06-jobs-api/middleware/error-handler.js
+++ 06-jobs-api/middleware/error-handler.js
@@ -1,5 +1,6 @@
 // const {CustomAPIError} = require('../errors')  dont need it since customError works
 const {StatusCodes} = require('http-status-codes')
+
 const errorHandlerMiddleware = async (err, req, res, next) => {
   
   let customError = {
@@ -18,11 +19,16 @@ const errorHandlerMiddleware = async (err, req, res, next) => {
     customError.statusCode = 400
   }
 
+
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate Value entered for ${Object.keys(err.keyValue)} field, please choose another value`
     customError.statusCode = 400
   }
 
+   if(err.name ==='CastError'){
+    customError.msg = `No item found with id : ${err.value}`
+    customError.statusCode = 404
+   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
