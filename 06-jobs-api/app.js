098b16ef7fb2f6e148ad5272ce41e51bd77781ca
Bobbylee007 <bobbyleeaustine@gmail.com>
2025-09-12 10:41:53 +0100

    security and deployment

--- 06-jobs-api/app.js
+++ 06-jobs-api/app.js
@@ -5,7 +5,7 @@ require('express-async-errors')
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require ('xss-clean')
-const rateLimit = require('express-rate-limit')
+const rateLimiter = require('express-rate-limit')
 
 
 
@@ -30,7 +30,18 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 const auth = require('./middleware/authentication')
 
 //middleware
+app.set('trust proxy', 1 )
+app.use(rateLimiter({
+    window: 15 * 60 *1000, // 15 minutes
+    max: 100, // limit each IP to requests per windowMs
+})
+);
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
 // extra packages
 
 
